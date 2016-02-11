22833d10363e001aa7a0ea0b1cceb5a4787beb0f
dengyun <me@leidengyun.cn>
2016-02-11 15:47:54 +0800

    rebuilding site 2016年02月11日 周四 上午  7:47:54

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
-set datevar=%date:~0,4%%date:~5,2%%date:~8,2%
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
 git add -A
-git commit -m "new article"%$datevar%
+git commit -m "$msg"
 git push origin master
 
 
@@ -14,7 +20,7 @@ git add -A
 
 # Commit changes.
 
-git commit -m "new article"%$datevar%
+git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
